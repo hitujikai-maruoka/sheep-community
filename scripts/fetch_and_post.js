c91f38be201f6caefec12dea4a128922ff9d31f2
hitujikai-maruoka <hitujikai.maruoka@gmail.com>
2025-07-19 01:09:06 +0900

    Sanity本文に引用元リンクを自動挿入

--- scripts/fetch_and_post.js
+++ scripts/fetch_and_post.js
@@ -61,6 +61,7 @@ async function main() {
     await postToSanity({
       title: item.title,
       summary: '', // 要約なし
+      body: `引用元: [${item.title}](${item.link})`,
       url: item.link,
       tags
     });
